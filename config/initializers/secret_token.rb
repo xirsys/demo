d32566dc3db222072c9b96a162b164f117b97c92
Alex Thomas <alex@xirsys.com>
2014-02-07 00:18:39 +0000

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+XirsysRails::Application.config.secret_key_base = '7eb3206d9ad6e65d855366749d04274b418f057ef4ca5a038b934971919ec7ea43783d7b8d59fe36b6e8c521673e456c9e781131c588a9696a086782b226c48d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+XirsysRails::Application.config.session_store :cookie_store, key: '_xirsys-rails_session'
